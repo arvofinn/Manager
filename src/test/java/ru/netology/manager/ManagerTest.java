fdb36420ead45b800ae610cdcd4aa05430f94d0c
Arvo <arvofinland@gmail.com>
2023-04-11 23:49:21 +0300

    add manager code and tests

---
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -0,0 +1,50 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ManagerTest {
+
+    @Test
+
+    public void test() {
+
+        String film1 = "Film I";
+        String film2 = "Film II";
+        String film3 = "Film III";
+
+        Manager manager = new Manager();
+
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+
+        String[] expected = { "Film I", "Film II", "Film III" };
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void test2() {
+
+        String film1 = "Film I";
+        String film2 = "Film II";
+        String film3 = "Film III";
+
+        Manager manager = new Manager();
+
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+
+        String[] expected = { "Film III", "Film II", "Film I" };
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
